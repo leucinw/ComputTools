da89fbba60f115612ac3f87417007caf2f0e863c
Chengwen Liu <liuchw2010@gmail.com>
2020-01-16 17:35:35 -0600

    changed dynamic.sh

--- bardemo/dynamic.sh
+++ bardemo/dynamic.sh
@@ -7,5 +7,5 @@ export CUDA_VISIBLE_DEVICES=0 # device number
 export OPENMMHOME=/home/liuchw/Softwares/tinkers/Tinker-OpenMM/AMOEBA/Tinker8.5/source
 T=298.15
 
-# NVT simulations, 2fs time step, save every 3 ps, 5ns in total, 298 K
+# NVT simulations, 2fs time step, save every 2 ps, 0.1ns in total, 298 K
 nohup $OPENMMHOME/dynamic_omm.x $filename.xyz -k $filename.key 50000 2.0 2.0 2 $T N > $filename.log 2>err.log & 
