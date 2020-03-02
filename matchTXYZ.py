2d87246af41182acf09dc34905c86fd86c9eb013
Chengwen Liu <liuchw2010@gmail.com>
2020-03-02 12:30:28 -0600

    sorted fingerprint

--- matchTXYZ.py
+++ matchTXYZ.py
@@ -6,16 +6,12 @@
 #   University of Texas at Austin  #
 #===================================
 
-# Using fingerprint to automatically match the atom type, this script has two functions:
-# 1. Generate a tinker-xyz file based on a "normal xyz" file and a template tinker-xyz file
-# 2. Generate a tinker-xyz file based on a "tinker xyz" file and a template tinker-xyz file
+# 1. Generate tinker-xyz by matching "normal-xyz to tinker-xyz"
+# 2. Generate tinker-xyz by matching "tinker-xyz to tinker-xyz" 
 
 ''' python matchTXYZ.py template.txyz dealwith.(t)xyz '''
 
 import os,sys
-#========================#
-# read tinker-xyz file   #
-#========================#
 def readTXYZ(TXYZ, singleAtom=False, tailOnly=False):
   atoms=[];coord=[]; tails = []
   order=[];types=[];connections=[]
@@ -27,32 +23,17 @@ def readTXYZ(TXYZ, singleAtom=False, tailOnly=False):
     idxstr = " " + data[5] + " "
     idx = line.index(idxstr)
     tails.append(line[idx:])
-    #=================================================
-    # sometime AA is "CA", it is Carbon, not Calcium 
-    # ================================================
     if singleAtom:
       atoms.append(data[1][0])
     else:
       atoms.append(data[1])
     coord.append([float(data[2]), float(data[3]), float(data[4])])
-  #=======================================#
-  # return only connection and type info. #
-  #=======================================#
   if tailOnly:
     return tails
-  #=======================================#
-  # return all info. of a tinker-xyz file #
-  #=======================================#
   else:
     return atoms,coord,order,types,connections
 
-#=============================================
-# use .mna provided by obabel as fingerprint
-#=============================================
 def fingerprint(TXYZ):
-  #===========================================#
-  # add some comments if no comments in txyz  #
-  #===========================================#
   lines = open(TXYZ).readlines()
   if len(lines[0].split()) == 1:
     with open(TXYZ, 'w') as f:
@@ -62,25 +43,20 @@ def fingerprint(TXYZ):
   fp = []
   MNA = TXYZ.replace("txyz", "mna")
   obstr = "obabel -itxyz %s -omna -O %s"%(TXYZ, MNA)
-  print(obstr)
   os.system(obstr)
   for line in open(MNA).readlines()[2:]:
-    fpstr = ''.join(sorted(line[:-1])) 
-    fp.append(fpstr)
+    idx = line.index("(")
+    atm = line[0:idx]
+    fpstr = ''.join(sorted(line[idx:-1])) 
+    fp.append(atm + fpstr)
   return fp
 
-#=======================================================#
-# make a xyz2txyz convertion if normal xyz is provided  #
-#=======================================================#
 def xyz2txyz(xyz):
   txyz = xyz.replace("xyz", "txyz")
   obstr = "obabel -ixyz %s -otxyz -O %s"%(xyz, txyz)
   os.system(obstr)
   return
 
-#===============#
-# main function #
-#===============#
 def main():
   template = sys.argv[1]
   dealwith = sys.argv[2]
