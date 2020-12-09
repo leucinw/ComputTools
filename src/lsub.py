671df840e00950b8a18f0e6c3d33bce81df3134f
Chengwen Liu <liuchw2010@gmail.com>
2020-12-08 22:47:18 -0600

    add memory upper bound for better selecting nodes

--- src/lsub.py
+++ src/lsub.py
@@ -9,8 +9,8 @@ import os,time,sys,subprocess
 import argparse
 import numpy as np
 import concurrent.futures 
+from datetime import datetime
 
-# nodes to check
 def checkone(node):
   njobs = 0
   exelist = ["psi4","g09","g16","dynamic.x","dynamic", "cp2k.ssmp","mpirun_qchem","orca_mp2_mpi","gmx_mpi","gmx"]
@@ -25,14 +25,14 @@ def checkone(node):
 
 def checkNodes(nodes):
   nodejobs = []
-  print('\033[91m' + "Checking availability of RENLAB clusters..." + '\033[0m')
+  now = datetime.now().strftime("%b %d %Y %H:%M:%S")
+  print('\033[91m' + "[" + now + "] " + "Checking availability of RENLAB clusters..." + '\033[0m')
   with concurrent.futures.ProcessPoolExecutor() as executor:
     results = [executor.submit(checkone, node) for node in nodes]
     for f in concurrent.futures.as_completed(results):
       nodejobs.append(f.result())
   return nodejobs 
 
-# submit job one by one 
 def subOneJob(node,qmfile):
   f, ext = os.path.splitext(qmfile)
   cwd = os.getcwd()
@@ -50,51 +50,46 @@ def subOneJob(node,qmfile):
   subprocess.run(cmdstr,shell=True)
   return
 
-# prepare file&nodelist
-def prepare(flistin, scratch=300, memory=30):
+def prepare(flistin, scratch=300, memory=30, maxmem=999):
   flist = []; nlist = []
   nodes = np.loadtxt("/home/liuchw/shared/renlab.nodes", usecols=(0), unpack=True, dtype="str", skiprows=1) 
   memorys, cpus, scratches = np.loadtxt("/home/liuchw/shared/renlab.nodes", usecols=(1,2,3), unpack=True, dtype="int", skiprows=1)
-  #check if .log file exists!
   for f in flistin:
     if not os.path.isfile(os.path.splitext(f)[0] + ".log"):
       flist.append(f)
     else:
       print('\033[93m' + f"log file exists for {f}!" + '\033[0m') 
-  #select nodes according to scratch/memory
   for n, s, m in zip(nodes, scratches, memorys):
-    if (s > scratch) and (m > memory):
+    if (s > scratch) and (m > memory) and (m < maxmem):
       nlist.append(n)
   return flist, nlist
 
-# main
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('-i', dest = 'input',  nargs='+', help = "Input files [x.com, y.psi4, z.qchem]",    required=True)  
+  parser.add_argument('-i', dest = 'input',  nargs='+', help = "Input files",    required=True)  
   parser.add_argument('-n', dest = 'nodes',  nargs='+', help = "Submit jobs on these nodes ONLY. Default: []",default=[])  
-  parser.add_argument('-x', dest = 'nodes_', nargs='+', help = "Submit jobs NOT on these nodes. Default: []", default=[])  
+  parser.add_argument('-x', dest = 'xnodes', nargs='+', help = "Submit jobs NOT on these nodes. Default: []", default=[])  
   parser.add_argument('-d', dest = 'disk',   type =int, help = "Disk of requested nodes. Default: 200 (GB)",  default=200)  
-  parser.add_argument('-m', dest = 'memory', type =int, help = "Memory of requested nodes. Default: 30 (GB)", default=30)  
+  parser.add_argument('-m', dest = 'memory', type =int, help = "Memory lower bound. Default: 30 (GB)", default=30)  
+  parser.add_argument('-M', dest = 'maxmem', type =int, help = "Memory upper bound. Default: 999 (GB)", default=999)  
   args = vars(parser.parse_args())
   inps = args["input"]
   nodes = args["nodes"]
-  nodes_ = args["nodes_"]
+  xnodes = args["xnodes"]
   disk = args["disk"]
   memory = args["memory"] 
+  maxmem = args["maxmem"] 
 
-  # use nodes
   if (nodes == []):
-    qmfiles, nodes = prepare(inps, disk, memory)
+    qmfiles, nodes = prepare(inps, disk, memory, maxmem)
   else:
-    qmfiles, _ = prepare(inps, disk, memory)
+    qmfiles, _ = prepare(inps, disk, memory, maxmem)
 
-  # excluding nodes
-  if (nodes_ != []):
-    for ex in nodes_:
+  if (xnodes != []):
+    for ex in xnodes:
       if (ex in nodes):
         nodes.remove(ex)
 
-  # detect and submit jobs 
   jobidx = 0
   while(jobidx != len(qmfiles)):
     results = checkNodes(nodes)
@@ -113,8 +108,7 @@ def main():
       else:time.sleep(30.0)
   return
 
-# execute
-if __name__ == '__main__':
-  if len(sys.argv) == 1:
-    sys.exit('\033[93m' + " please use '-h' option to see usage" + '\033[0m')
+if len(sys.argv) == 1:
+  print('\033[93m' + " please use '-h' option to see usage" + '\033[0m')
+else:
   main()
