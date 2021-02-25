3eb42e607f3299f64d57ed9eb49fee9e3d511db4
Chengwen Liu <liuchw2010@gmail.com>
2021-02-25 10:28:39 -0600

    get faster speed using numpy

--- src/ldensity.py
+++ src/ldensity.py
@@ -8,10 +8,14 @@
 import numpy as np
 import subprocess
 import sys
+import os
 
 ''' Usage: python ldensity arcfile'''
 
 arcfile = sys.argv[1]
+
+if not os.path.isfile(arcfile):
+  sys.exit(f"{arcfile} does not exist!")
 
 massdict = {"H": 1.007941, "C":12.01074, "N":14.006703, "O":15.999405, "S":32.0648, "P":30.973761998, \
             "F":18.998403163, "CL":35.4529, "BR":79.9035,"NA":22.98976928, "K":39.0983, "MG":24.3051, "ZN":65.38 }
@@ -28,9 +32,6 @@ navogadro = 6.02214086 #x10**23
 for atom in atoms:
   mass+=massdict[atom.upper()]
 sumdens = 0.0
-for i in range(len(lattices)):
-  volume = lattices[i]**3
-  dens = 1000*10.0*(mass/navogadro)/volume
-  sumdens += dens
-  print("%20s%10.1f kg/m^3"%("Frame: %9d"%i, dens))
-print("%20s%10.1f kg/m^3" %("Average Density:", sumdens/len(lattices)))
+volume = lattices.mean()**3
+dens = 1000*10.0*(mass/navogadro)/volume
+print("%20s%10.1f kg/m^3" %("Average Density:", dens))
