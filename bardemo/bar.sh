d9170b0f317a25e436bb611cf8d58ccf2f3c6e94
Chengwen Liu <liuchw2010@gmail.com>
2021-04-07 10:59:45 -0500

    updated example folder

--- bardemo/bar.sh
+++ bardemo/bar.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-source /home/liuchw/.openmm.amoeba.cuda8.0 
-export filename=k_water
+source /home/liuchw/OpenMM-Nov-2020/openmm.bashrc
+export filename=liquid
+export CUDA_VISIBLE_DEVICES=0 
 export nextstep
-export CUDA_VISIBLE_DEVICES=0 # device number
-export OPENMMHOME=/home/liuchw/Softwares/tinkers/Tinker-OpenMM/AMOEBA/Tinker8.5/source
+export OPENMMHOME=/home/liuchw/OpenMM-Nov-2020/tinker/source/
 T=298.15
 
 nohup $OPENMMHOME/bar_omm.x 1 $filename.arc $T ../$nextstep/$filename.arc $T  >bar1.log 2>err1.log &&
--- bardemo/dynamic.sh
+++ bardemo/dynamic.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# To run on other cards, GTX1070 etc. if you decide to use this, comment out the above blocks 
-source /home/liuchw/.openmm.amoeba.cuda8.0 
-export filename=k_water
-export CUDA_VISIBLE_DEVICES=0 # device number
-export OPENMMHOME=/home/liuchw/Softwares/tinkers/Tinker-OpenMM/AMOEBA/Tinker8.5/source
-T=298.15
+source /home/liuchw/OpenMM-Nov-2020/openmm.bashrc
+export filename=liquid
+export CUDA_VISIBLE_DEVICES=0 
+export OPENMMHOME=/home/liuchw/OpenMM-Nov-2020/tinker/source/
+export T=298.15
 
-# NVT simulations, 2fs time step, save every 2 ps, 0.1ns in total, 298 K
 nohup $OPENMMHOME/dynamic_omm.x $filename.xyz -k $filename.key 50000 2.0 2.0 2 $T N > $filename.log 2>err.log & 
